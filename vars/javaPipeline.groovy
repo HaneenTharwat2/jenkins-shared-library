574d970a89918557def43d1c2055cb1564d862d4
HaneenTharwat2 <haneentharwat27@gmail.com>
2025-05-26 17:40:23 +0300

    Fix: cleaned javaPipeline.groovy

--- vars/javaPipeline.groovy
+++ vars/javaPipeline.groovy
@@ -1,15 +1,36 @@
 def call() {
     pipeline {
         agent any
+
+        tools {
+            jdk 'java-8'
+        }
+
+        environment {
+            DOCKER_USER = credentials('dockerhub-user')
+            DOCKER_PASS = credentials('dockerhub-password')
+        }
+
         stages {
             stage('Build') {
                 steps {
-                    sh './gradlew build'
+                    echo 'Building Java project...'
+                    sh './mvnw clean package'
                 }
             }
-            stage('Test') {
+
+            stage('Docker Build') {
                 steps {
-                    sh './gradlew test'
+                    echo 'Building Docker image...'
+                    sh "docker build -t my-java-image ."
+                }
+            }
+
+            stage('Docker Push') {
+                steps {
+                    echo 'Pushing Docker image...'
+                    sh "echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin"
+                    sh "docker push my-java-image"
                 }
             }
         }
