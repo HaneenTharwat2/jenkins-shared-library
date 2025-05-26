493c006e39903643584408cf840f9611662b7365
HaneenTharwat2 <153605899+HaneenTharwat2@users.noreply.github.com>
2025-05-27 01:58:09 +0300

    Update pipelineLib3.groovy

--- vars/pipelineLib3.groovy
+++ vars/pipelineLib3.groovy
@@ -4,26 +4,23 @@ def call() {
 
         environment {
             XYZ = 'ITI ITI ITI'
-            IMAGE_NAME = 'haneentharwat/jenkins'
-        }
+            IMAGE_NAME = 'hendsiam/jenkins'
+        }  
 
         stages {
             stage('Build & Dockerize') {
                 steps {
                     echo 'Building Docker image for Python app...'
-                    sh "docker build -t ${IMAGE_NAME}:latest ."
+                    sh 'docker build -t my-python-app .'
                 }
             }
 
             stage('Push Docker Image') {
                 steps {
                     echo 'Pushing Docker image to DockerHub'
-                    withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
-                        sh "echo \$DOCKER_PASSWORD | docker login -u \$DOCKER_USERNAME --password-stdin"
-                        sh "docker push ${IMAGE_NAME}:latest"
-                    }
+                    pushDockerImage(env.IMAGE_NAME)
                 }
             }
         }
-    }
+    }  
 }
